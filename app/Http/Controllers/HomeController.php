0498da5a3716224acfefd9975bcde8ff8a0a04b6
Edwin Ascencio <ar23052@ues.edu.sv>
2026-09-03 14:31:46 -0600

    Arreglando el entorno para empezar a trabajar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $products = \App\Models\Product::all();
-        return view('home', compact('products'));
+        return view('home');
     }
 }
